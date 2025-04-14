57e3900a6a02fe5fd9153360804744f313b59e76
Willemberg Bruno <willem.bruno@hotmail.com>
2025-04-14 11:26:37 -0300

    exercicio06

---
+++ exercicio06.py
@@ -0,0 +1,13 @@
+n1 = int(input("Digite a primeria nota: "))
+while n1 < 0 or n1 > 10 :
+    print("Primeira Nota Invalida!")
+    n1 = int(input("Digite a primeria nota: "))
+
+n2 = int(input("Digite a segunda nota: "))
+while n2 < 0 or n2 > 10 :
+    print("Segunda nota Invalida!!")
+    n2 = int(input("Digite a segunda nota: "))
+
+media = (n1+n2)/2
+
+print(f"A Média é: {media}")
